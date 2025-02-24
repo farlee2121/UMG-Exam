668c8bc04ee92ff122ae24a626db5b2fd29071a5
Spencer Farley <2847259+farlee2121@users.noreply.github.com>
2025-02-24 09:07:38 -0600

    Make a documentation project

---
+++ Documentation/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventStore.Client;
+using EventStore;
+
+namespace Documentation
+{
+    internal class Class1
+    {
+        public void Boi()
+        {
+            string connectionString = "esdb://127.0.0.1:2113?tls=false&keepAliveTimeout=10000&keepAliveInterval=10000";
+            var clientSettings = EventStoreClientSettings.Create(connectionString);
+            var client = new EventStorePersistentSubscriptionsClient(clientSettings);
+
+            //client.su
+            //client
+
+        }
+
+        
+    }
+}
